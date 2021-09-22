0ffec09e287be4aa2344f129e5ca2171b17d7322
Rutger Pels <rutger_pels@hotmail.com>
2021-09-22 13:51:22 +0200

    adjusted Yaml file

--- Scripts/Deployment.ps1
+++ Scripts/Deployment.ps1
@@ -1,2 +1,5 @@
 New-AzResourceGroup -Name Dev-Pels-02 -Location "West Europe"
 New-AzResourceGroupDeployment -ResourceGroupName Dev-Pels-02 -TemplateFile main.bicep -Confirm -verbose
+
+Select-AzSubscription fffb0c65-e90a-4b5d-adac-a5d6d399e2cc
+New-AzResourceGroup -Name Dev-Pels-02 -Location "West Europe" 
