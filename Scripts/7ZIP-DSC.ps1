b5ba95868a5f29c9253a26998b048a5caac82a4d
Rutger Pels <rutger.pels@i-share.nl>
2021-09-24 15:28:04 +0200

    fixed bug for dsc

--- Scripts/7ZIP-DSC.ps1
+++ Scripts/7ZIP-DSC.ps1
@@ -1,12 +1,17 @@
 Configuration Install7Zip {
-    Node localhost {
-        {
-            Package 7zip {
-                Ensure     = 'Present'
-                Name       = '7-Zip 19.00 (x64 edition)'
-                Path       = 'C:\Users\rutge\Downloads\7z1900-x64.msi'
-                ProductId  = '23170F69-40C1-2702-1900-000001000000'
-            }
+
+    # Import the module that contains the resources we're using.
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+
+    # The Node statement specifies which targets this configuration will be applied to.
+    Node 'localhost' {
+
+        # The first resource block ensures that the Web-Server (IIS) feature is enabled.
+        Package 7zip {
+            Ensure    = "Present"
+            Name      = "7-Zip 19.00"
+            Path      = "C:\Local\7z1900-x64.msi"
+            ProductId = "23170F69-40C1-2702-1900-000001000000"
         }
     }
 }
